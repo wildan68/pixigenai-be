e972285b0744b24094f069b6494d880b108fd4c0
Wildan Rizky <wildanrizkiyadi68@gmail.com>
2024-09-22 11:04:44 +0700

    adjustment rate limiter timeout

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -104,7 +104,7 @@ export function isAdmin (req: Request, res: Response) {
 }
 
 export const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minutes
+  windowMs: 10 * 60 * 1000, // 10 minutes
   max: 100, // limit each IP to 100 requests per windowMs
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
